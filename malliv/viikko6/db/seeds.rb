b42019fd2624bb142d00fb57fe21232af5a261e1
Matti Luukkainen <mluukkai@iki.fi>
2016-03-01 15:03:30 +0200

    Update seeds.rb

--- malliv/viikko6/db/seeds.rb
+++ malliv/viikko6/db/seeds.rb
@@ -8,11 +8,3 @@
 b1 = Brewery.create name:"Koff", year:1897
 b2 = Brewery.create name:"Malmgard", year:2001
 b3 = Brewery.create name:"Weihenstephaner", year:1042
-
-b1.beers.create name:"Iso 3", style:"Lager"
-b1.beers.create name:"Karhu", style:"Lager"
-b1.beers.create name:"Tuplahumala", style:"Lager"
-b2.beers.create name:"Huvila Pale Ale", style:"Pale Ale"
-b2.beers.create name:"X Porter", style:"Porter"
-b3.beers.create name:"Hefeweizen", style:"Weizen"
-b3.beers.create name:"Helles", style:"Lager"
